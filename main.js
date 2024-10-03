d3bdf2fa9f4f6d4b086280f7b1ff0156408d18ae
Seif Samir <117075048+seif-samir89@users.noreply.github.com>
2024-10-04 00:32:18 +0300

    new

--- main.js
+++ main.js
@@ -19,7 +19,6 @@ async function getQuotes() {
   const data = await response.json();
   return data;
 }
-
 async function generateQuote() {
   const quotes = await getQuotes();
   const quote = quotes[Math.floor(Math.random() * quotes.length)];
@@ -51,34 +50,33 @@ function copyText() {
     });
 }
 //Download image
-// the best code
-// ==========================================
-
 downloadBtn.addEventListener("click", () => {
-  const imageUrl = quoteImg.src; 
-  const fileName = `Image_Quote_${quoteId.innerText}`; 
+  const imageUrl = quoteImg.src;
+  const fileName = `Image_Quote_${quoteId.innerText}`;
 
   fetch(imageUrl)
     .then((response) => {
       if (!response.ok) {
         throw new Error(" Network response was not ok");
       }
-      return response.blob();    
+      return response.blob();
     })
     .then((blob) => {
       const url = window.URL.createObjectURL(blob);
       const a = document.createElement("a");
       a.href = url;
-      a.download = fileName; 
+      a.download = fileName;
       document.body.appendChild(a);
-      a.click(); 
-      document.body.removeChild(a); 
-      window.URL.revokeObjectURL(url); 
+      a.click();
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
     })
     .catch((error) => {
       console.error("Failed to download image:", error);
       alert("Failed to download image. Please try again.");
     });
 });
-
-// ================================
+// Loading
+window.addEventListener("load", function () {
+  document.querySelector(".loading").classList.add("hide");
+});
